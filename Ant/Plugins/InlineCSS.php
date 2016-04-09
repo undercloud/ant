61a0589fd921d7d6b42e4fdbd7f871a3dcd1b941
mooden <alexandrostruses@yandex.ru>
2016-04-10 01:53:38 +0400

    improve

---
+++ Ant/Plugins/InlineCSS.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Ant\Plugins;
+
+/*
+ * Inline CSS generator
+ */
+class InlineCSS extends Base
+{
+	/**
+	 * Register plugin
+	 *
+	 * @param Ant\Ant $ant instance
+	 *
+	 * @return void
+	 */
+	public function register($ant)
+	{
+		$ant->register('css', function($storage) {
+			return new \Undercloud\InlineCSS($storage);
+		});
+	}
+}
+?>
